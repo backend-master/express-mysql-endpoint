2f183a89707e98007ec71f4c46cd380c08519566
ayogatot <azerino25@gmail.com>
2019-08-22 12:54:16 +0700

    update book conrollers endpoint

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -31,3 +31,71 @@ exports.deteleDatabase = (req, res) => {
     }
   });
 };
+
+exports.createBooks = (req, res) => {
+  const { name, author } = req.body;
+  const sql = `INSERT INTO books (name, author) VALUES (?,?)`;
+  db.query(sql, [name, author], (err, result) => {
+    if (err) {
+      return console.log(err);
+    } else {
+      res.status(200).json({
+        err: false,
+        errMessage: null,
+        data: {
+          id: result.insertId
+        }
+      });
+    }
+  });
+};
+
+exports.getAllBooks = (req, res) => {
+  const sql = `SELECT * FROM books`;
+  db.query(sql, [], (err, result) => {
+    if (err) {
+      return console.log(err);
+    } else {
+      res.status(200).json({
+        err: false,
+        errMessage: null,
+        data: result
+      });
+    }
+  });
+};
+
+exports.getBookById = (req, res) => {
+  const id = req.params.id;
+  const sql = `SELECT * FROM books WHERE id= ?`;
+  db.query(sql, id, (err, result) => {
+    if (err) {
+      return console.log(err);
+    } else {
+      res.status(200).json({
+        err: false,
+        errMessage: null,
+        data: result
+      });
+    }
+  });
+};
+
+exports.deleteBookById = (req, res) => {
+  const id = req.params.id;
+  const sql = `DELETE FROM books WHERE id= ?`;
+  db.query(sql, id, (err, result) => {
+    if (err) {
+      return console.log(err);
+    } else {
+      res.status(200).json({
+        err: false,
+        errMessage: null,
+        data: {
+          id: id,
+          msg: `Book has been deleted => ${result}`
+        }
+      });
+    }
+  });
+};
--- routes/books.js
+++ routes/books.js
@@ -6,4 +6,12 @@ const bookController = require("../controllers/bookController");
 Router.get("/createTB", bookController.createDatabase);
 Router.get("/deleteTB", bookController.deteleDatabase);
 
+Router.route("/")
+  .post(bookController.createBooks)
+  .get(bookController.getAllBooks);
+
+Router.route("/:id")
+  .get(bookController.getBookById)
+  .delete(bookController.deleteBookById);
+
 module.exports = Router;
